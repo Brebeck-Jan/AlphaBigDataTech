c641c82a0f1066252ee80968d774ef11d61e0d2f
Phantomias3782 <andreas@mmacb.de>
2020-08-20 12:50:29 +0200

    Added working mongo

--- BigDataArchitecture/delete.sh
+++ BigDataArchitecture/delete.sh
@@ -1,10 +1,13 @@
 kubectl delete svc/app-service
 kubectl delete svc/mongo-service
 kubectl delete svc/memcached-service
+kubectl delete svc/headless-service
 
 kubectl delete deployment/app-deployment
 kubectl delete deployment/memcache-deployment
 kubectl delete deployment/mongo-deployment
+
+kubectl delete statefulset/mongo
 
 kubectl delete horizontalpodautoscaler.autoscaling/app-deployment
 kubectl delete horizontalpodautoscaler.autoscaling/memcache-deployment
