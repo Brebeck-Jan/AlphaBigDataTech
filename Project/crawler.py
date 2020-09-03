b6b1a3163a37c976276cc5034026d09b1df7ff27
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-09-03 17:10:57 +0200

    modified crawler

--- Project/crawler.py
+++ Project/crawler.py
@@ -4,13 +4,12 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
 
 def crawl_page(url):
-    """ Crawl nes of given page and save to Datalake.
+    """ Crawl news of given page and save to Datalake.
         Base structure from older project: https://github.com/Brebeck-Jan/alpha-tech/blob/master/code/backend/System/NewsCrawler.py
     """
-    print(url)
-    print(type(url))
     # set headers and cookies to prevent to not get blacklistet
     headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
@@ -24,20 +23,38 @@ def crawl_page(url):
 
     # parse data
     data = BeautifulSoup(response.content, "html.parser")
-
     # get titles of data
-    titles = data.find_all("title")
+    items = data.find_all("item")
+    articles = []
     
-    # extract only title
-    titles = [re.search("<title>(.*)</title>", str(element)).group(1) for element in titles]
+    for item in items:
+        article = {}
+        article["site"]=url.split(".")[1]
+        article["time"]=datetime.now().strftime("%H:%M:%S")
+        article["title"] = item.title.get_text()
+        article["link"] = item.guid.get_text()
 
-    print(titles)
+        article["text"] = ""
+        article_side =  BeautifulSoup(requests.request("GET", article["link"], headers = headers, cookies = cookies).content, "html.parser")
+        if "spiegel" in url:
+            selector = 'div[class*="RichText"]'
+            article["authors"] = [author.get_text() for author in article_side.select('div[class="font-sansUI lg:text-base md:text-base sm:text-s text-shade-dark"]')]
+            article["tags"] = [cat.get_text() for cat in item.find_all("category")]
+        elif "faz" in url:
+            selector = 'p[class*="TextParagraph"]'
+            article["authors"] = [author.get_text() for author in article_side.select('li[class="atc-MetaItem atc-MetaItem-author"]')]
+            article["tags"] = [url.split("/")[-2]]
+        for paragraph in article_side.select(selector):
+            article["text"] =  article["text"] + paragraph.get_text()
+        articles.append(article)
+        
     # save to hadoop
-    save_to_hadoop(titles)
+    save_to_hadoop(articles,url)
 
-def save_to_hadoop(titles):
-    "Function to save titles to hadoop."
-    pass
+def save_to_hadoop(articles,url):
+    with open("./output/"+articles[0]["site"]+url.split(".")[2].split("/")[-2]+".txt","w",encoding="utf-8") as f:
+        for article in articles:
+            f.write(str(article))
 
 def run_all():
     "Run crawling for all saved newspages."
@@ -45,11 +62,7 @@ def run_all():
     with open("newspages.txt") as f:
 
         for newspage in f.readlines():
-            
             # run crawler
             crawl_page(newspage.rstrip())
-
-# crawl_page("https://www.spiegel.de/schlagzeilen/index.rss")
-# crawl_page("http://newsfeed.zeit.de/all")
 
 run_all()
